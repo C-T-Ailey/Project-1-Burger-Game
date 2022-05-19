3df073e9641e81f59b8dc8ed867e2af06e1e917b
Chris Ailey <chris.ailey90@gmail.com>
2022-05-19 17:16:48 +0100

    added multiple ingredients, implemented incremental speed on level progression, added styling

--- scripts/main.js
+++ scripts/main.js
@@ -12,10 +12,16 @@ function range(num1,num2){
 }
 
 function areEqual(array1, array2) {
+    //console.log(array1.length)
+    //console.log(array2.length)
     if (array1.length === array2.length) {
-      return array1.every((element, index) => {
-        if (element === array2[index]) {
-          return true;
+        return array1.every((element, index) => {
+            elementClass = element.getAttribute("class");
+            arr2Class = array2[index].getAttribute("class")
+            //console.log(elementClass)
+            //console.log(arr2Class)
+            if (elementClass === arr2Class) {
+                return true;
         }
   
         return false;
@@ -26,10 +32,14 @@ function areEqual(array1, array2) {
   }
 
 var burg = document.getElementById("burger");
+console.log(burg.childNodes)
 var bottomBun = document.querySelector("#bun-bottom");
 var startButton = document.querySelector("#stButton");
 //console.log(startButton)
-var gameArea = document.querySelector("#gameWindow")
+var gameArea = document.querySelector("#gameWindow");
+var titleDiv = document.getElementById("title")
+var titleText = document.getElementById("textTitle")
+var correctOrders = document.querySelector("#correct")
 //console.log(gameArea)
 
 //console.log(document.getElementById("pipe-1").offsetLeft);
@@ -51,84 +61,130 @@ var startPos = 0
 burg.style.left = locArr[startPos]+"px";
 
 
-
 const patty = document.createElement("div");
+const patty2 = document.createElement("div");
+const patty3 = document.createElement("div");
+patties = [patty, patty2, patty3];
 patty.setAttribute("id","patty");
-patty.style.position = "absolute";
-patty.style.left = locArr[0]+"px";
-patty.style.top = "50px";
-patty.style.height = "10px";
-patty.style.width = "90px";
-patty.style.marginLeft = "15px";
-patty.style.backgroundColor = "rgb(56, 38, 26)";
-//console.log("Patty left pos " + patty.style.left)
-//console.log( "burger xpos " + burg.style.left)
-//gameArea.appendChild(patty)
-//patty.style.display = "none";
-
-/*burg.appendChild(patty);
-patty.style.position = "absolute";
-patty.style.left = "0px"
-patty.style.marginLeft = "5px"
-patty.style.top = "80%";*/
+patties.forEach(element => {
+    element.setAttribute("class","patty");
+    element.style.position = "absolute";
+    element.style.left = locArr[0]+"px";
+    element.style.top = "50px";
+    element.style.height = "10px";
+    element.style.width = "90px";
+    element.style.marginLeft = "15px";
+    element.style.backgroundColor = "rgb(56, 38, 26)";
+});
+console.log("Patties! ", patties)
 
 const tomato = document.createElement("div");
 const tomato2 = document.createElement("div");
+const tomato3 = document.createElement("div");
+tomatoes = [tomato, tomato2, tomato3];
 tomato.setAttribute("id","tomato");
-tomato.style.position = "absolute";
-tomato.style.left = locArr[0]+"px";
-tomato.style.top = "50px";
-tomato.style.height = "10px";
-tomato.style.width = "50px";
-tomato.style.marginLeft = "35px";
-tomato.style.backgroundColor = "red";
+tomatoes.forEach(element =>{
+    element.setAttribute("class", "tomato");
+    element.style.position = "absolute";
+    element.style.left = locArr[0]+"px";
+    element.style.top = "50px";
+    element.style.height = "10px";
+    element.style.width = "50px";
+    element.style.marginLeft = "35px";
+    element.style.backgroundColor = "red";
+});
+console.log("Tomatoes!", tomatoes)
 
 const cheese = document.createElement("div");
+const cheese2 = document.createElement("div");
+const cheese3 = document.createElement("div");
+cheeses = [cheese, cheese2, cheese3];
 cheese.setAttribute("id","cheese");
-cheese.style.position = "absolute";
-cheese.style.left = locArr[0]+"px";
-cheese.style.top = "50px";
-cheese.style.height = "5px";
-cheese.style.width = "70px";
-cheese.style.marginLeft = "25px";
-cheese.style.backgroundColor = "orange";
+cheeses.forEach(element => {
+    element.setAttribute("class", "cheese")
+    element.style.position = "absolute";
+    element.style.left = locArr[0]+"px";
+    element.style.top = "50px";
+    element.style.height = "5px";
+    element.style.width = "70px";
+    element.style.marginLeft = "25px";
+    element.style.backgroundColor = "orange";
+});
+console.log("Cheeses christ! ", cheeses)
 
 const lettuce = document.createElement("div");
+const lettuce2 = document.createElement("div");
+const lettuce3 = document.createElement("div");
+lettuces = [lettuce, lettuce2, lettuce3]
 lettuce.setAttribute("id","lettuce");
-lettuce.style.position = "absolute";
-lettuce.style.left = locArr[0]+"px";
-lettuce.style.top = "50px";
-lettuce.style.height = "5px";
-lettuce.style.width = "85px";
-lettuce.style.marginLeft = "17px";
-lettuce.style.backgroundColor = "lawngreen";
+lettuces.forEach(element => {
+    element.setAttribute("class", "lettuce")
+    element.style.position = "absolute";
+    element.style.left = locArr[0]+"px";
+    element.style.top = "50px";
+    element.style.height = "5px";
+    element.style.width = "85px";
+    element.style.marginLeft = "17px";
+    element.style.backgroundColor = "lawngreen";
+});
+console.log("Lettuce pray!", lettuces)
 
 const topBun = document.createElement("div");
+const topBun2 = document.createElement("div");
+topBuns = [topBun, topBun2]
 topBun.setAttribute("id","topBun");
-topBun.style.position = "absolute";
-topBun.style.top = "35px";
-topBun.style.marginLeft = "10px";
-topBun.style.height = "20px";
-topBun.style.width = "100px";
-topBun.style.borderRadius = "150px 150px 0 0";
-topBun.style.backgroundColor = "rgb(232, 167, 83)";
+topBuns.forEach(element => {
+    element.setAttribute("class", "topBun")
+    element.style.position = "absolute";
+    element.style.top = "35px";
+    element.style.marginLeft = "10px";
+    element.style.height = "20px";
+    element.style.width = "100px";
+    element.style.borderRadius = "150px 150px 0 0";
+    element.style.backgroundColor = "rgb(232, 167, 83)";
+});
 
 
 
 let ingredients=[patty, cheese, tomato, lettuce, topBun]
 let levelIngredients = {
-    level1: [patty, cheese, tomato, lettuce, topBun],
-    level2: [patty, patty, lettuce, lettuce, cheese, topBun]
+    level1: [patty, patty2, patty3, cheese, cheese2, cheese3, tomato, tomato2, tomato3, lettuce, lettuce2, lettuce3, topBun, topBun2]
+
+    //level2: [patty, patty2, patty3, cheese, cheese2, cheese3, tomato, tomato2, tomato3, lettuce, lettuce2, lettuce3, topBun]
 }
 
-//let arr = []
+function restock(){
+    levelIngredients.level1 = [patty, patty2, patty3, cheese, cheese2, cheese3, tomato, tomato2, tomato3, lettuce, lettuce2, lettuce3, topBun,topBun2]
+    //levelIngredients.level2 = [patty, patty2, patty3, cheese, cheese2, cheese3, tomato, tomato2, tomato3, lettuce, lettuce2, lettuce3, topBun]
+}
 
 const target = {
     level1: [bottomBun, patty, cheese, lettuce, tomato, topBun],
     level2: [bottomBun, lettuce, patty, cheese, lettuce, patty, cheese, topBun]
 }
 
+
+console.log("level 1 ingredients list")
+/*burgArr.forEach(element => {
+    console.log(element.getAttribute("class"))
+});
+
+console.log("")
+console.log("level 2 ingredients list")
+target.level1.forEach(element => {
+    if (element.getAttribute("id")!="bun-bottom"){
+        console.log(element.getAttribute("id"))
+    }
+});*/
+
 burgArr = [bottomBun]
+
+/*function compareResult(array1,array2){
+    arr1 = []
+    array1.forEach(element => {
+        
+    });
+}*/
 
 document.onkeydown = moveBurg;
 function moveBurg(e) {
@@ -138,54 +194,48 @@ function moveBurg(e) {
         burg.style.left = locArr[startPos-1]+"px";
         startPos -= 1
         console.log("leftpress")
-        //console.log("burg offset " + burg.offsetLeft)
-        //console.log("locArr[startPos]" + locArr[startPos])
-        //console.log((burg.offsetLeft-10)===locArr[startPos])
-        //bottomBun.style.left  = 100+"px";
-        //console.log(bottomBun.style.left)
     }
     else if (e.keyCode == '39' && startPos != 3) {
+        // right arrow
         burg.style.left = locArr[startPos+1]+"px";
         startPos += 1
-        //console.log("rightpress")
-        //console.log("burg offset " + burg.offsetLeft)
-        //console.log("locArr[startPos]" + locArr[startPos])
-        //console.log((burg.offsetLeft-10)===locArr[startPos])
-
-        // right arrow
-        //bottomBun.style.left  = (posLeft+10)+"px";
-        //console.log(bottomBun.style.left)
+        console.log("rightpress")
+        
     }
     else if ((e.keyCode == '37' || e.keyCode == '39') && (startPos === 0 || startPos === 3)){
         console.log("You're at the end of the counter! You'll drop the burger!")
     }
 }
 
-
-var currentLevel = 1;     //Set the ingredients array for the current level
+var speed = 2;
+var currentLevel = 0;
 
 function startGame(){
+    
     startButton.style.display = "none";     //Hide the start button
+    titleDiv.style.display = "none";
     console.log("")
-    console.log("Current Level: ", currentLevel)
+    //console.log("Current Level: ", currentLevel)
+    
     function setIngredients(){
         if (currentLevel===1){
             console.log("level1")
             return levelIngredients.level1;
         }
         else if (currentLevel===2){
-            console.log(levelIngredients.level2)
+            console.log("level2")
             return levelIngredients.level2;
         }
     }
-    var currentIngredients = setIngredients();
-    console.log("Level ingredients: ", currentIngredients)
+    var currentIngredients = levelIngredients.level1;
+    //var currentIngredients = setIngredients();
+    //console.log("Level ingredients: ", currentIngredients)
     
     
     function rollIngredient(){
         return currentIngredients[Math.floor(Math.random()*(currentIngredients.length))];
     }      // Generate the ingredient to drop
-
+    
     function rollDropLocation(){
         return locArr[Math.floor(Math.random()*(locArr.length))];
     }       // Generate the pipe to drop ingredient from
@@ -199,8 +249,13 @@ function startGame(){
     var randomLoc = rollDropLocation();
     var xPos = randomLoc+"px";
     
-    //ingredient.style.left = xPos;
 
+    function speedIncrease(){
+        return speed += .5;
+    }
+    
+    //ingredient.style.left = xPos;
+    
     //console.log("array " + randomLoc)
     //console.log("xpos" + xPos)
     //var ingredientPos = locArr.indexOf(randomLoc)
@@ -212,9 +267,9 @@ function startGame(){
 
     var id = setInterval(drop, 5) 
     function drop(){
-        do {
+        //do {
             gameArea.appendChild(ingredient);
-            yPos+=2;
+            yPos+=speed;
             //console.log(catchCap)
             //console.log(gameArea.offsetHeight)
             ingredient.style.top = yPos+"px"
@@ -259,44 +314,96 @@ function startGame(){
                 ingredient.style.top = positionTop+"px";
                 let removeIndex = currentIngredients.indexOf(ingredient)
                 currentIngredients.splice(removeIndex,1);
-                console.log("after splice: ", currentIngredients)
+                //console.log("after splice: ", currentIngredients)
                 //startButton.style.display = "inline-block";
-                if (currentIngredients.length > 0) {
-                    startGame()
-                    //console.log("new loop")
-                } else {
-                    levelFinished = true;
-                    console.log("Level finished!")
+                //levelFinished = true;
+                if(areEqual(target.level1, burgArr)===true){
+                    console.log("Level finished!")                    
                     clearInterval(id)
-                    if(areEqual(target.level1, burgArr)===true){
+                    //if(areEqual(target.level1, burgArr)===true){
                         console.log("Congratulations! You got the order right.")
                         startButton.style.display = "inline-block";
-                        startButton.innerHTML = "You got the order right!<br>Next Level"
+                        titleDiv.style.display = "block";
+                        titleText.innerHTML = "Another satisfied customer!"
+                        startButton.innerHTML = "Next, please!"
                         currentLevel += 1
+                        correctOrders.innerHTML = `Correct orders: ${currentLevel}`;
+                        speedIncrease();
+                        nextLevel = true;
                         burgArr = [bottomBun];
-                        levelIngredients.level1 = [patty, cheese, tomato, lettuce, topBun]
-                        levelIngredients.level1.forEach(element => {
-                            burg.removeChild(element)
-                        });
-                        console.log(currentLevel)
-                    } else {
-                        console.log("You messed up, man! You're fired!")
-                        startButton.innerHTML = "You got the order wrong!<br>Start Again"
-                        currentLevel = 1;
-                        //console.log(burgArr)
-                        burgArr = [bottomBun]
-                        levelIngredients.level1 = [patty, cheese, tomato, lettuce, topBun]
-                        levelIngredients.level1.forEach(element => {
-                            burg.removeChild(element)
-                        });
-                        console.log(levelIngredients.level1)
+                        restock()
+                        let burgLength = burg.childElementCount
+                        //console.log(burgLength)
+                        //console.log(burg.children.length)
+                        //console.log(burg.children)
+                        for (i=burgLength;i>1;i--){
+                            //console.log("to remove: ", burg.children[1])
+                            burg.removeChild(burg.children[1])
+                            //console.log("Remaining removals: ", burg.children)
+                            //console.log("remaining elements: ", burgLength)
+                        }
+                        burgLength = burg.childElementCount
+                        //console.log(burgLength)
+                        //console.log(burg)
+                        console.log("Current level: ",currentLevel)
+                        console.log("Speed = ", speed)
                         
                         startButton.style.display = "inline-block";
+                } else if (burg.childElementCount > target.level1.length) {
+                    console.log("Level finished!")                    
+                    clearInterval(id)
+                    console.log("You messed up, man! You're fired!")
+                    startButton.style.display = "inline-block";
+                    titleDiv.style.display = "block";
+                    titleText.innerHTML = "You're fired!"
+                    startButton.innerHTML = "New Hire"
+                    currentLevel = 0;
+                    correctOrders.innerHTML = "Correct orders: 0";
+                    console.log(currentLevel)
+                    speed = 2;
+                    console.log(speed)
+                    //console.log(burgArr)
+                    burgArr = [bottomBun]
+                    restock()
+                    let burgLength = burg.childElementCount
+                    //console.log(burgLength)
+                    //console.log(burg.children.length)
+                    //console.log(burg.children)
+                    for (i=burgLength;i>1;i--){
+                        //console.log("to remove: ", burg.children[1])
+                        burg.removeChild(burg.children[1])
+                        //console.log("Remaining removals: ", burg.children)
+                        //console.log("remaining elements: ", burgLength)
+                        
+                    }
+                    burgLength = burg.childElementCount
+                    //console.log(burgLength)
+                    //console.log(burg)
+                    /*burg.childNodes.forEach(element => {
+                        if (element != bottomBun && element.innerText != " "){
+                            console.log(element)
+                            burg.removeChild(element)
+                            console.log("Child removed")
+                        }
+                    })*/
 
+                    /*levelIngredients.level1.forEach(element => {
+                        burg.removeChild(element)
+                    });
+                    console.log(levelIngredients.level1)*/
+                    
+                    
+                    
+//                    } else if (currentIngredients.length > 0) {
+                    } else {
+
+                        startGame()
+                        //console.log("new loop")
                     }
                 }
             }
-        } while (levelFinished = false); 
+        }
+        //} while (levelFinished = false); 
 
         /*if (catchZone.includes(yPos) && burg.offsetLeft-10===randomLoc){
             //console.log(tomato.style.height)
@@ -352,8 +459,8 @@ function startGame(){
             yPos++;
             ingredient.style.top = yPos+"px"
         }*/
-    }
-}
+    //}
+//}
 
 //console.log("gameArea " + gameArea.offsetHeight)
 
